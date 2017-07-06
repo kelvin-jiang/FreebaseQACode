29ffc6c273e56a3c2c902c93fe5b062137c6a038
infinitecold <kelvin_322@ymail.com>
2017-07-06 11:45:23 -0400

    temporarily commented out previous classes to test mysql

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,28 @@
 
 public class Main {
-    private static final int TEST_INDEX = 237;
+    //private static final int TEST_INDEX = 211;
 
-    public static void main(String args[]) {
-        String FILEPATH = args[0];
+    public static void main(String[] args) {
+        /*String FILEPATH = args[0];
 
+        //retrieve QA from JSON
         RetrieveQA retrieval = new RetrieveQA(FILEPATH);
         //int size = retrieval.getSize(); unused
         String[] questions = retrieval.getQuestions();
         String[] answers = retrieval.getAnswers();
 
-        System.out.println(questions[TEST_INDEX]);
+        //tag Q
+        for (String question : questions) {
+            System.out.println(question);
+            TagMe tagger = new TagMe(question);
+            String[] questionTags = tagger.retrieveEntities();
+            for (String questionTag : questionTags) {
+                System.out.println(questionTag);
+            }
+            System.out.println("\n");
+        }*/
 
-        TagMe tagger = new TagMe(questions[TEST_INDEX]);
-        String[] questionTags = tagger.retrieveEntities();
+        //MySQL
 
-        for (String questionTag : questionTags) {
-            System.out.println(questionTag);
-        }
     }
 }
