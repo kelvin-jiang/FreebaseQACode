bf934d1411b2a7622cb665a39e97e35bfb47063e
infinitecold <kelvin_322@ymail.com>
2017-07-13 12:24:37 -0400

    completed top-down + bottom-up search

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Main {
-    private static final int TEST_INDEX = 2;
-    private static final double RHO_THRESHOLD = 0.15;
+    private static final int TEST_INDEX = 37;
+    private static final double RHO_THRESHOLD = 0.2;
 
     public static void main(String[] args) {
         String FILEPATH = args[0]; //take command line argument as JSON file to be read
 
         //retrieve QA from JSON
         RetrieveQA retrieval = new RetrieveQA(FILEPATH);
-        //int size = retrieval.getSize(); unused
+        //int size = retrieval.getSize(); //unused
         String[] questions = retrieval.getQuestions();
         String[] answers = retrieval.getAnswers();
 
@@ -37,11 +38,36 @@ public class Main {
         //Freebase
         FreebaseDBHandler db = new FreebaseDBHandler();
 
-        //test query/update
-        /*db.updateTable("ALTER TABLE freebase_mysql_db.`freebase-onlymid_-_id2en_name` ADD PRIMARY KEY (`freebase_mid`)");
-        //System.out.println(db.parseQueryResult(2));*/
+        //top-down + bottom-up search
+        //List<String> answerIDsList = db.nameAlias2IDs(answer);
+        HashSet<String> answerIDs = new HashSet<>();
+        answerIDs.addAll(db.nameAlias2IDs(answer)); //prepares all freebase IDs with a name or alias matching the answer
+        System.out.println(answerIDs);
 
-        for (String tag : questionTags) { //repeats for each tag
+        for (String tag : questionTags) {
+            List<String> tagIDs = db.nameAlias2IDs(tag);
+            for (String tagID : tagIDs) {
+                List<NTriple> triples = db.ID2Triples(tagID);
+                for (NTriple triple : triples) {
+                    if (answerIDs.contains(triple.getFormattedObjectID())) { //if the object of the triple has an ID matching an answer ID, check its name
+                        List<String> namesAliases = db.objectID2NamesAliases(triple.getFormattedObjectID()); //gets names/aliases from object of current triple
+                        triple.setSubject(tag);
+                        for (String nameAlias : namesAliases) { //repeats for each name/alias
+                            triple.setObject(nameAlias); //adds object name to triple
+                            System.out.println(triple.toString());
+                            if (nameAlias.toLowerCase().trim().equals(answer.toLowerCase().trim())) {
+                                String[] saveData = {triple.getSubject(), triple.getSubjectID(), triple.getFormattedPredicate(), triple.getFormattedObjectID(),
+                                        triple.getObject(), question, answer};
+                                System.out.println("TO SAVE: " + Arrays.toString(saveData));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //top-down search
+        /*for (String tag : questionTags) { //repeats for each tag
             List<String> tagIDs = db.nameAlias2IDs(tag);
             for (String tagID : tagIDs) { //repeats for each tag ID
                 List<NTriple> triples = db.ID2Triples(tagID);
@@ -59,6 +85,6 @@ public class Main {
                     }
                 }
             }
-        }
+        }*/
     }
 }
