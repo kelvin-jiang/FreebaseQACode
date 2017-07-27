a82cd52031f0d24fdf8b13b3fe2c72138d9d65b7
infinitecold <kelvin_322@ymail.com>
2017-07-26 23:04:08 -0400

    integrated fofe ner

--- src/FOFE.py
+++ src/FOFE.py
@@ -5,7 +5,6 @@ data = {
     'mode' : 'dev',
     'lang' : 'English'
 }
-
 input_filepath = sys.argv[1]
 index = input_filepath.find(".txt")
 output_filepath = input_filepath[:index] + "-FOFE" + input_filepath[index:] #filename.txt -> filename-fofe.txt
@@ -13,27 +12,34 @@ output_filepath = input_filepath[:index] + "-FOFE" + input_filepath[index:] #fil
 with open(input_filepath, 'r') as inputfile:
     with open(output_filepath, 'w') as outputfile:
         for line in inputfile:
-            data['text'] = line.split(" | ")[0]
+            data['text'] = line.split(" | ")[0]  # set text in data to question to be tagged
             r = requests.post(url, data=data)
             rawjson = json.loads(r.text)
-            del rawjson['second_pass']
+
+            indicators = ['first_pass_hidden', 'first_pass_shown']
+            entities = []
             tags = []
-            for i in rawjson:
-                for j in i:
-                    for k in j:
-                        tags.append(k['entities'])
-            print(tags)
+            for indicator in indicators:
+                for i in range(0, len(rawjson[indicator])):
+                    index = '%s' % str(i)  # formats index in 'index' form
+                    entities = rawjson[indicator][index]['entities']
+                    for entity in entities:
+                        entity.append(rawjson[indicator][index]['text'])
+                        if len(entity) == 5 and "NOM" not in entity[1]:
+                            tags.append(entity)
 
-            #tags = rawjson["first_pass_hidden"]["0"]["entities"]
-            #tags.extend(rawjson["first_pass_shown"]["0"]["entities"])
-            #print(tags)
-
+            entities = []
             for tag in tags:
-                if 'NOM' in tag[1]:
-                    tags.remove(tag)
+                #entity = []
+                entity = tag[4][tag[2][0][0]:tag[2][0][1]]
+                #entity.append(tag[3])
+                entities.append(entity)
 
-            print("real: " + str(tags))
-
-            #line = line.replace("\n", " | added\n")
-            #outputfile.write(line)
-
+            # TODO: need to search entity in wikipedia and append that
+            extension = ""
+            for entity in entities:
+                extension = extension + " | " + entity + " | " + entity
+            line = line.rstrip() + extension
+            print(line)
+            outputfile.write(line  + "\n")
+print("FOFE TAGGING COMPLETE")
