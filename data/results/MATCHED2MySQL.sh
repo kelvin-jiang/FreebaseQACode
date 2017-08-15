63c570ad577785fa086c85bb6df3a85e9204a587
Kelvin Jiang <kelvin@navy.eecs.yorku.ca>
2017-08-15 13:51:36 -0400

    added remaining data files

--- data/results/MATCHED2MySQL.sh
+++
@@ -1,2 +0,0 @@
-( cat triviaQA-qa-TagMe-ToFreeBase-MATCHED1.txt | awk -F "|" '{print $1 "|" $2 "|" $3 "|" $4 "|" " null " "|" $5 "|" $6 "|" $7}' ; cat triviaQA-qa-TagMe-ToFreeBase-MATCHED2.txt | cut -d'|' -f1-4,6-9 ) | sort -t'|' -k8 > triviaQA-qa-ready4MySQL.txt
-
